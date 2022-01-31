86bd72755d11b8e7da8c6ead9e9c351981783d8c
riscaldo <riccardo85ferrara@gmail.com>
2022-01-31 16:58:03 +0700

    better structure

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,37 +17,30 @@ router.post('/login', (req, res) => {
 })
 
 router.post('/signup', async(req, res, next) => {
-    let user = req.body
-    Users.findOne({
-        'email': user.email
-    }, async function(err, exist) {
-        try {
-            // hanlde err..
-            // IF THE USER ALREADY EXISTS
-            if (exist) {
-                throw new Error('A user with this email is already registered')
-            } else
-            // IF THE USER DO NOT EXISTS
-            {
-                // create the user
-                let users = await Users.create(user)
-                console.log('user created!')
-                    //login
-                req.login(user, (err) => {
-                    if (err) {
-                        throw err
-                    } else {
-                        console.log('logged in!')
-                            // after login go to the houses list page
-                        res.redirect('/houses')
-                    }
-                })
-
-            }
-        } catch (err) {
-            next(err)
+    try {
+        let user = await Users.findOne({
+            'email': user.email
+        })
+        if (user) {
+            throw new Error('A user with this email is already registered')
+        } else {
+            let user = await Users.create(req.body)
+            console.log('user created!')
+                //login
+            req.login(user, (err) => {
+                if (err) {
+                    throw err
+                } else {
+                    console.log('logged in!')
+                        // after login go to the houses list page
+                    res.redirect('/houses')
+                }
+            })
         }
-    })
+
+    } catch (err) {
+        next(err)
+    }
 
 })
 
