c60cd202b2033d78bc70737a868d8e59ef97617a
riscaldo <riccardo85ferrara@gmail.com>
2022-01-31 15:51:07 +0700

    createUser is a function

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,22 +3,38 @@ const express = require('express')
 const router = express.Router()
 const Users = require('../models/users')
 
+
+//functions 
+const createUser = async(user) => {
+    let users = await Users.create(user)
+    return true
+}
+
 //Create requests GET / POST
 router.get('/login', (req, res) => {
     res.render('login')
 })
+
 router.get('/signup', (req, res) => {
     res.render('signup')
 })
+
 router.post('/login', (req, res) => {
     res.send('login')
 })
+
 router.post('/signup', async(req, res) => {
-    console.log(req.body)
-    let users = await Users.create(req.body)
-    await console.log(Users.find({}))
-    res.send('signup')
+    let user = req.body
+    let created = await createUser(user)
+    res.send('signed up')
+        // if (created) {
+        //     req.login({
+        //         email: 'tony@gmail.com',
+        //         password: '123456'
+        //     })
+        // }
 })
+
 router.get('/logout', (req, res) => {
     res.send('logout')
 })
