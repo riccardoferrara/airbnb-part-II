08fad78d40a4dbdea6bf8f695d26fbc00c3df499
riscaldo <riccardo85ferrara@gmail.com>
2022-01-31 15:38:17 +0700

    create new user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 // Import Packages
 const express = require('express')
 const router = express.Router()
+const Users = require('../models/users')
 
 //Create requests GET / POST
 router.get('/login', (req, res) => {
@@ -12,7 +13,10 @@ router.get('/signup', (req, res) => {
 router.post('/login', (req, res) => {
     res.send('login')
 })
-router.post('/signup', (req, res) => {
+router.post('/signup', async(req, res) => {
+    console.log(req.body)
+    let users = await Users.create(req.body)
+    await console.log(Users.find({}))
     res.send('signup')
 })
 router.get('/logout', (req, res) => {
