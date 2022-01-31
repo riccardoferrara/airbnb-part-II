af350b4f09bbbfd3eddac3768c11d62ef0570775
riscaldo <riccardo85ferrara@gmail.com>
2022-01-31 12:13:35 +0700

    add houses requests templates

--- controllers/houses.js
+++ controllers/houses.js
@@ -6,5 +6,24 @@ const router = express.Router()
 router.get('/', (req, res) => {
     res.send('houses')
 })
+router.get('/create', (req, res) => {
+    res.send('houses')
+})
+router.get('/:id', (req, res) => {
+    res.send('houses')
+})
+router.get('/:id/edit', (req, res) => {
+    res.send('houses')
+})
+router.post('/', (req, res) => {
+    res.send('houses')
+})
+router.patch('/:id', (req, res) => {
+    res.send('houses')
+})
+router.delete('/:id', (req, res) => {
+    res.send('houses')
+})
+
 
 module.exports = router
