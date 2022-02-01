27bb9e16f3c0b1a4e4319cac91a12108f0ce0603
riscaldo <riccardo85ferrara@gmail.com>
2022-02-01 12:18:51 +0700

    just more readable code

--- controllers/houses.js
+++ controllers/houses.js
@@ -6,6 +6,7 @@ const router = express.Router()
 router.get('/', (req, res) => {
     res.render('houses/list')
 })
+
 router.get('/create', (req, res, next) => {
     try {
         if (!req.isAuthenticated()) {
@@ -15,18 +16,23 @@ router.get('/create', (req, res, next) => {
         }
     } catch (err) { next(err) }
 })
+
 router.get('/:id', (req, res) => {
     res.send('houses/one')
 })
+
 router.get('/:id/edit', (req, res) => {
     res.render('houses/edit')
 })
+
 router.post('/', (req, res) => {
     res.render('houses/list')
 })
+
 router.patch('/:id', (req, res) => {
     res.send('houses')
 })
+
 router.delete('/:id', (req, res) => {
     res.send('houses')
 })
