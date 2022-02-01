877e8174846f33ed44b8c34d9d80160771f08b92
riscaldo <riccardo85ferrara@gmail.com>
2022-02-01 12:34:51 +0700

    protect profile requests from !auth users

--- controllers/profile.js
+++ controllers/profile.js
@@ -3,11 +3,24 @@ const express = require('express')
 const router = express.Router()
 
 //Create requests GET / POST
-router.get('/', (req, res) => {
-    res.render('profile')
+router.get('/', (req, res, next) => {
+    try {
+        if (!req.isAuthenticated()) {
+            res.render('auth/login')
+        } else {
+            res.render('profile')
+        }
+    } catch (err) { next(err) }
 })
-router.patch('/', (req, res) => {
-    res.send('profile')
+
+router.patch('/', (req, res, next) => {
+    try {
+        if (!req.isAuthenticated()) {
+            res.render('auth/login')
+        } else {
+            res.send('profile')
+        }
+    } catch (err) { next(err) }
 })
 
 module.exports = router
