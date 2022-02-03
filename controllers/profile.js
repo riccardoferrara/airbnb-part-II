e83603a45dc8194b0ed5a4a57a64b4a984f70a00
riscaldo <riccardo85ferrara@gmail.com>
2022-02-03 14:08:16 +0700

    Update User Profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,6 +1,20 @@
 // Import Packages
 const express = require('express')
+const { update } = require('../models/users')
 const router = express.Router()
+const Users = require('../models/users')
+
+// Functions
+const loginOrErr = (req, res, user) => {
+    req.login(user, (err) => {
+        if (err) {
+            throw err
+        } else {
+            console.log('logged in!')
+                // after login go to the houses list page
+        }
+    })
+}
 
 //Create requests GET / POST
 router.get('/', (req, res, next) => {
@@ -15,12 +29,23 @@ router.get('/', (req, res, next) => {
     } catch (err) { next(err) }
 })
 
-router.patch('/', (req, res, next) => {
+router.patch('/', async(req, res, next) => {
     try {
         if (!req.isAuthenticated()) {
+            console.log('not logged')
             res.render('auth/login')
         } else {
-            res.send('profile')
+            console.log('patching user informations')
+                // user info from update request
+            updated_user = req.body
+            console.log('updated user: ', updated_user)
+                // update database
+            users = await Users.findOneAndUpdate(updated_user)
+                // login out user and login with new values
+            req.logout()
+            loginOrErr(req, res, updated_user)
+            console.log('after update logged user is: ', req.user)
+            res.redirect('profile')
         }
     } catch (err) { next(err) }
 })
