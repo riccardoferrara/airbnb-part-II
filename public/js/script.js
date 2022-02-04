6c9ae15aa31bc304b2c60a184b9baca30ef96abd
riscaldo <riccardo85ferrara@gmail.com>
2022-02-04 18:42:55 +0700

    make the large image of the gallery become the same image as the thumbnail that you click on.

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,7 @@
-const updateMainPhoto = () => {
-    console.log('here we are')
+// in the one house page, whe click on thumbnail, main photo is updated 
+const updateMainPhoto = (element) => {
+    // find the src of the clicked photo
+    let src = element.style.backgroundImage
+        // change the main photo src
+    document.querySelector('.main_img').style.backgroundImage = src
 }
